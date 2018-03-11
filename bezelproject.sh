f621a86aaefa49dc47e5100d2cc28af5f5c193b5
thebezelproject <thebezelproject@gmail.com>
2018-03-10 19:32:16 -0600

    Updated NES

--- bezelproject.sh
+++ bezelproject.sh
@@ -490,16 +490,16 @@ nes)
     cp /tmp/retroarch.cfg /opt/retropie/configs/nes/retroarch.cfg
     sed -i '2i input_overlay = "/opt/retropie/configs/all/retroarch/overlay/Nintendo-Entertainment-System.cfg"' /opt/retropie/configs/nes/retroarch.cfg
     sed -i '3i input_overlay_opacity = "1.000000"' /opt/retropie/configs/nes/retroarch.cfg
-    sed -i '4i aspect_ratio_index = "16"' /opt/retropie/configs/atarilynx/retroarch.cfg
-    sed -i '5i video_force_aspect = "true"' /opt/retropie/configs/atarilynx/retroarch.cfg
-    sed -i '6i video_aspect_ratio = "-1.000000"' /opt/retropie/configs/atarilynx/retroarch.cfg
+    sed -i '4i aspect_ratio_index = "16"' /opt/retropie/configs/nes/retroarch.cfg
+    sed -i '5i video_force_aspect = "true"' /opt/retropie/configs/nes/retroarch.cfg
+    sed -i '6i video_aspect_ratio = "-1.000000"' /opt/retropie/configs/nes/retroarch.cfg
   else
     cp /opt/retropie/configs/nes/retroarch.cfg /opt/retropie/configs/nes/retroarch.cfg.bkp
     sed -i '2i input_overlay = "/opt/retropie/configs/all/retroarch/overlay/Nintendo-Entertainment-System.cfg"' /opt/retropie/configs/nes/retroarch.cfg
     sed -i '3i input_overlay_opacity = "1.000000"' /opt/retropie/configs/nes/retroarch.cfg
-    sed -i '4i aspect_ratio_index = "16"' /opt/retropie/configs/atarilynx/retroarch.cfg
-    sed -i '5i video_force_aspect = "true"' /opt/retropie/configs/atarilynx/retroarch.cfg
-    sed -i '6i video_aspect_ratio = "-1.000000"' /opt/retropie/configs/atarilynx/retroarch.cfg
+    sed -i '4i aspect_ratio_index = "16"' /opt/retropie/configs/nes/retroarch.cfg
+    sed -i '5i video_force_aspect = "true"' /opt/retropie/configs/nes/retroarch.cfg
+    sed -i '6i video_aspect_ratio = "-1.000000"' /opt/retropie/configs/nes/retroarch.cfg
   fi
   ;;
 pce-cd)
